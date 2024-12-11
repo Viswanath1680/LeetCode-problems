e31ee7c83d18d47a1b2a03f83ba10a90fd45c7a1
Viswanath1680 <127508141+Viswanath1680@users.noreply.github.com>
2024-12-11 23:31:52 +0530

    Time: 0 ms (100%), Space: 31.1 MB (7.55%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& arr) {
-        int n = arr.size();
-        int l, r;
-        int num = INT_MAX;
-        for( int i = 0; i < n; i++ ){
-            if( arr[i] * arr[i] < num ){
-                num = arr[i] * arr[i];
-                l = i;
-            }
-        }
-
-        r = l+1;
-        vector<int>answer;
-        while( l >= 0 && r < n ){
-            if( abs( arr[l] ) < abs( arr[r] ) ){
-                answer.emplace_back( arr[l] * arr[l] );
-                l--;
+    vector<int> sortedSquares(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        vector<int> answer;
+        while( l <= r ){
+            if( abs(nums[l]) > abs(nums[r]) ){
+                answer.emplace_back( nums[l] * nums[l] );
+                l++;
             }
             else{
-                answer.emplace_back( arr[r] * arr[r] );
-                r++;
+                answer.emplace_back( nums[r] * nums[r] );
+                r--;
             }
         }
-        while( l >= 0 )  answer.emplace_back( arr[l] * arr[l--] );
-        while( r < n )  answer.emplace_back( arr[r] * arr[r++] );
-
+        reverse( answer.begin(), answer.end() );
         return answer;
     }
 };
