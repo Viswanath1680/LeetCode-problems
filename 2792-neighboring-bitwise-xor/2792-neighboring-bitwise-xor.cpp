ff13d8901d70a46bb296ba8c6845a44e9d0b1065
Viswanath1680 <127508141+Viswanath1680@users.noreply.github.com>
2025-01-18 00:07:38 +0530

    Time: 23 ms (11.68%), Space: 277.9 MB (17.29%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
-    bool doesValidArrayExist(vector<int>& derived) {
-        vector<int> original = {0};
-        for (int i = 0; i < derived.size(); i++) {
-            original.push_back((derived[i] ^ original[i]));
+    bool checkWithZero( vector<int> arr ){
+        // Assuming first element is 0
+        // Derive the remaining elements. And check if x_n^x_1 == derived[n-1]
+        int n = arr.size();
+        vector<int>temp(n);
+        temp[0] = 0;
+        for( int i = 1; i < n; i++ ){
+            temp[i] = arr[i-1] ^ temp[i-1];
         }
-        bool checkForZero = (original[0] == original[original.size() - 1]);
-        original = {1};
-        for (int i = 0; i < derived.size(); i++) {
-            original.push_back((derived[i] ^ original[i]));
-        }
-        bool checkForOne = (original[0] == original[original.size() - 1]);
+        return ( (temp[n-1] ^ temp[0]) == arr[n-1] );
+    }
 
-        return checkForZero | checkForOne;
+    bool checkWithOne(vector<int> arr){
+        int n = arr.size();
+        vector<int>temp(n);
+        temp[0] = 1;
+        for( int i = 1; i < n; i++ ){
+            temp[i] = arr[i-1] ^ temp[i-1];
+        }
+        return ( (temp[n-1] ^ temp[0]) == arr[n-1] );
+    }
+
+    bool doesValidArrayExist(vector<int>& derived) {
+        // base check condition
+        int temp_xor = 0;
+        for( auto it : derived )    temp_xor ^= it;
+        if( temp_xor ) return false;
+
+        bool b1 = checkWithZero( derived );
+        if( b1 )    return true;
+        bool b2 = checkWithOne( derived );
+        return b2;
     }
 };
