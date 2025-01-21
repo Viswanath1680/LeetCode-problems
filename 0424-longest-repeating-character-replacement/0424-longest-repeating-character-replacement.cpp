29fd2e6aea3cceb4a09e7a185e30cc55e22dcc64
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-01-22 01:14:19 +0530

    Time: 16 ms (18.93%), Space: 9.8 MB (99.58%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,32 @@
+/*
+Window can have atmost k chars which are not equal to most frequent character, for the answer to be maximum. Our goal is to maximise the len of the window.
+*/
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0, r = 0, len = 0, maxi = 0, ans = 0;
+        // len is length of the window, which is (r-l+1)
+        map<char, int> mp;
+        while( r < s.length() ){
+            mp[ s[r] ]++;
+            len++;
+            maxi = max(maxi, mp[ s[r] ]);   // most frequent char in the window
+            if( len - maxi <= k )    ans = max(ans, len );
+            else if( len - maxi > k ){
+                while( len - maxi > k ){
+                    mp[ s[l] ]--;
+                    l++;
+                    len--;
+                    int temp_max = INT_MIN;
+                    for( auto it : mp ){
+                        temp_max = max( temp_max, it.second );
+                    }
+                    maxi = temp_max;
+                }
+            }
+            r++;
+        }   
+        return ans;
+    }
+};
