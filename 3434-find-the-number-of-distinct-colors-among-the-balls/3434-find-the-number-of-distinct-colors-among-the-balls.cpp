0972889b0638df284f00eeab73764153f64190b8
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-02-07 15:52:53 +0530

    Time: 188 ms (29.9%), Space: 171.9 MB (18.01%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
-        unordered_map<int,int> ball, color;
-        vector<int> ans;
-        ans.reserve(queries.size());
-        int distinct = 0;
-        for(auto &q : queries) {
-            int pos = q[0], c = q[1];
-            if(ball.count(pos))
-                if(--color[ball[pos]] == 0) distinct--;
-            ball[pos] = c;
-            if(++color[c] == 1) distinct++;
-            ans.push_back(distinct);
+    vector<int> queryResults(int n, vector<vector<int>>& queries) {
+        unordered_map<int, int> ball, color;
+        // ball[i] -> i is the index of the ball and ball[i] is the current color of the ball
+        // color[i] -> number of balls with color[i] as their color. 
+        vector<int>ans;
+        // arr[0] -> ball number
+        // arr[1] -> ball color
+        for( auto arr : queries ){
+            int paint = arr[1];
+            // first time painting the ball
+            if( ball.find( arr[0] ) == ball.end() ){
+                ball[ arr[0] ] = paint;
+                color[ arr[1] ]++;
+                ans.emplace_back( color.size() );
+            }
+            // repainting the ball
+            else{
+                int prev_paint = ball[ arr[0] ];
+                color[ prev_paint ]--;
+                if( color[prev_paint] == 0 )    color.erase( prev_paint );
+                ball[ arr[0] ] = paint;
+                color[ arr[1] ]++;
+                ans.emplace_back(color.size());
+            }
         }
         return ans;
     }
