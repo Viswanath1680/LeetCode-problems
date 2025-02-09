0c3f29b8c2a54c1f85d96b8fbdcde8838edc2f55
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-02-09 16:29:19 +0530

    Time: 66 ms (70.36%), Space: 87.4 MB (87.39%) - LeetHub

---
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long nC2( int m ){
+        long long n = m;
+        long long ans = ( n*(n-1) )/2;
+        return ans;
+    }
+
+    long long countBadPairs(vector<int>& arr) {
+        int n = arr.size();
+        int valid = 1;
+        unordered_map<int, int> ump;
+        for( int i = 0; i < n; i++ ){
+            arr[i] -= i;
+            ump[arr[i]]++;
+        }
+        long long ans = nC2(n);
+        for( auto it : ump ){
+            ans -= nC2( it.second );
+        }
+        return ans;
+    }
+};
