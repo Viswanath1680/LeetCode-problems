19185184c50f31987b9279358241050930aa584c
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-02-10 14:40:27 +0530

    Time: 0 ms (100%), Space: 9.3 MB (9.61%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        while (s.find_first_of("0123456789") != std::string::npos) {
-            for (size_t i = 0; i < s.length(); ++i) {
-                if (isdigit(s[i])) {
-                    for (int j = i - 1; j >= 0; --j) {
-                        if (!isdigit(s[j])) {
-                            s.erase(i, 1);
-                            s.erase(j, 1);
-                            break;
-                        }
-                    }
-                    break;
-                }
+        int n = s.length();
+        stack<int> st;
+        vector<bool> arr(n, false);
+        for( int i = 0; i < n; i++ ){
+            if( s[i] - '0' >= 0 && s[i] - '0' <= 9 ){
+                int ind = st.top();
+                st.pop();
+                arr[ind] = true;
+                arr[i] = true;
             }
+            else    st.push(i);
         }
-        return s;
+        string ans = "";
+        for( int i = 0; i < n; i++ ){
+            if( arr[i] == false )   ans += s[i];
         }
-};
+        return ans;
+    }
+};  
