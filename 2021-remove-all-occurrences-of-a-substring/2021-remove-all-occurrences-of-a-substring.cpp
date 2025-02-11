d041e40de4434e9f65e2b3396ba5dc72d9557368
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-02-11 19:15:25 +0530

    Time: 10 ms (5.85%), Space: 20.9 MB (5.32%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // maintain an ans string. While it's length is < k(k == part.length()), push s[i] into it.
+    // Once >=k, check the latest k chars and pop them if equal to part.
     bool isequal( string s, string part ){
         int n = s.length(), k = part.length();
         if( n < k ) return false;
@@ -12,14 +14,7 @@ public:
         int n = s.length(), k = part.length();
         if( n < k ) return s;
         string ans = "";
-        // base case
-        for( int i = 0; i < k; i++ )    ans += s[i];
-        if( isequal(ans, part) ){
-            int d = k;
-            while(d--)  ans.pop_back();
-        }
-        // now length is >= k
-        for( int i = k; i < n; i++ ){
+        for( int i = 0; i < n; i++ ){
             ans += s[i];
             if( ans.length() >= k ){
                 if( isequal(ans, part) ){
