50f089a7f0bb3750fb9b8717b90901d3b8be744b
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-04-21 13:21:09 +0530

    Time: 6 ms (26.61%), Space: 112.6 MB (47.58%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int numberOfArrays(vector<int>& differences, int lower, int upper) {
         int n = differences.size();
-        vector<long long> prefix(n, 0);
-        prefix[0] = differences[0];
-        for( int i = 1; i < n; i++ )    prefix[i] = prefix[i-1] +(long long)differences[i];
+        // Idea is to calculate prefix sum
         //  example case -> 1 -2 2
         //  (least number in differences+ hidden[0]) should be >= lower
         //  and ( max number in differences + hidden[0] should be <= upper)
         long long min_val = (long long)lower, max_val = (long long)upper;   // min and max vals for hidden[0]
-        for( auto v : prefix ){
-            if( v + min_val < lower ){
-                long long diff = lower - (v+ min_val);
+        long long prefix_sum = 0;
+        for( auto v : differences ){
+            prefix_sum += (long long) v;
+            if( prefix_sum + min_val < lower ){
+                long long diff = lower - (prefix_sum+ min_val);
                 min_val += diff;
             }
-            if( v + max_val > upper ){
-                long long diff = ( v + max_val )  - upper;
+            if( prefix_sum + max_val > upper ){
+                long long diff = ( prefix_sum + max_val )  - upper;
                 max_val -= diff;
             }
         }
