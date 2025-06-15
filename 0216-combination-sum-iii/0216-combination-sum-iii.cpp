ea9fe7acedda094a17f4077f21e54efb5b7f2158
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-06-15 22:13:17 +0530

    Time: 0 ms (100%), Space: 8.8 MB (60.53%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-    void recursive(vector<vector<int>>&ans, vector<int>arr, vector<int>& temp, int i, int target, int k, int total ){
-        if( temp.size() > k )   return;
-        if( total == target ){
-            if( temp.size() == k ) ans.emplace_back(temp);
-            return;
-        }
-        if( i == 9 || total > target )  return;
-        // pick arr[i]
-        temp.emplace_back(arr[i]);
-        recursive(ans, arr, temp, i+1, target, k, total+arr[i]);
-        temp.pop_back();
-
-        // don't pick arr[i]
-        recursive(ans, arr, temp, i+1, target, k, total);
-    }
-
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
         int low = k, high = k*9;
@@ -24,7 +8,25 @@ public:
         vector<int> arr(9, 1);
         for( int i = 1; i < 9; i++ )    arr[i] += arr[i-1];
         vector<int>temp;
-        recursive(ans, arr, temp, 0, n, k, 0);
+
+        auto recursive = [&](auto& self, int i, int total) -> void {
+            if( temp.size() > k )   return;
+            if( total == n ){
+                if( temp.size() == k ) ans.emplace_back(temp);
+                return;
+            }
+            if( i == 9 || total > n )  return;
+            // pick arr[i]
+            temp.emplace_back(arr[i]);
+            self(self, i+1, total+arr[i]);
+            temp.pop_back();
+
+            // don't pick arr[i]
+            self(self, i+1, total);
+        };
+
+        recursive(recursive, 0, 0);
+
         return ans;
     }
 };
