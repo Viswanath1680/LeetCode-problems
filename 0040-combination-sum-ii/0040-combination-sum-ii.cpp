8df1558355f5f5f11df5c3e37f432d92f9648c6a
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-06-15 21:16:41 +0530

    Time: 0 ms (100%), Space: 13.9 MB (83.12%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,24 +1,29 @@
-
-void findCombination(int ind, int target, vector < int > & arr, vector < vector < int >> & ans, vector < int > & ds) {
-  if (target == 0) {
-    ans.push_back(ds);
-    return;
-  }
-  for (int i = ind; i < arr.size(); i++) {
-    if (i > ind && arr[i] == arr[i - 1]) continue;
-    if (arr[i] > target) break;
-    ds.push_back(arr[i]);
-    findCombination(i + 1, target - arr[i], arr, ans, ds);
-    ds.pop_back();
-  }
-}
 class Solution {
-public:  
-        vector < vector < int >> combinationSum2(vector < int > & candidates, int target) {
-        sort(candidates.begin(), candidates.end());
-        vector < vector < int >> ans;
-        vector < int > ds;
-        findCombination(0, target, candidates, ans, ds);
-        return ans;
+public:
+    // Neetcode Solution
+    void recursive(vector<vector<int>>& ans, vector<int>& arr, vector<int>& temp, int i, int target, int total){
+        int n = arr.size();
+        if( target == total ){
+           ans.emplace_back(temp);
+           return ; 
         }
+        if( i == n || total > target )  return;
+        
+        // pick arr[i]
+        temp.emplace_back(arr[i]);
+        recursive(ans, arr, temp, i+1, target, total+arr[i]);
+        temp.pop_back();
+
+        // don't pick arr[i] -> skip all the duplicate vals of arr[i]
+        while( i+1 < n && arr[i] == arr[i+1] )  i++;
+        recursive(ans, arr, temp, i+1, target, total);
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        vector<int> temp;
+        recursive(ans, candidates, temp, 0, target, 0);
+        return ans;
+    }
 };
