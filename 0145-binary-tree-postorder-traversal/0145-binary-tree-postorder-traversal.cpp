ffc5ad25e3542539cca806a5fe604d6bb900ac2c
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-06-23 22:55:56 +0530

    Time: 0 ms (100%), Space: 11 MB (70.91%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,26 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void postOrder( TreeNode* root, vector<int>& arr ){
-        if( root == nullptr )   return;
-        postOrder( root->left, arr );
-        postOrder( root->right, arr );
-        arr.emplace_back( root->val );
-    }
-
+    // recursive using lambda function
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        postOrder( root, ans );
-        return ans;        
+        vector<int>ans;
+        if( !root ) return ans;
+        auto recursive = [&](auto& self, TreeNode* root) -> void{
+            if( !root ) return;
+            self(self, root->left);
+            self(self, root->right);
+            ans.emplace_back(root->val);
+        };
+        recursive(recursive, root);
+        return ans;
     }
 };
