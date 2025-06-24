c6dad570b8d0e5b293c2e41fd4c30a094a669cef
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-06-25 01:20:55 +0530

    Time: 0 ms (100%), Space: 15 MB (62.25%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,37 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<vector<int>> levelOrder;
-        vector<int> ans;
+        vector<int>ans;
         if( !root ) return ans;
-        list<TreeNode*> l;
-        l.push_back( root );
-        while( l.size() ){
-            vector<int> temp;
-            int n = l.size();
-            for( int i = 0; i < n; i++ ){
-                TreeNode* tempnode = l.front();
-                l.pop_front();
-                temp.push_back( tempnode->val );
-                if( tempnode->left )    l.push_back( tempnode->left );
-                if( tempnode->right )   l.push_back( tempnode->right );
+        queue<TreeNode*> q;
+        q.push(root);
+        while( q.size() ){
+            int n = q.size();
+            TreeNode* lastnode;
+            while( n-- ){
+                auto node = q.front();
+                q.pop();
+                lastnode = node;
+                if( node->left )    q.push(node->left);
+                if( node->right )   q.push(node->right);
             }
-            levelOrder.push_back( temp );
-        }
-        for( auto arr : levelOrder ){
-            int n = arr.size();
-            ans.push_back( arr[n-1] );
+            ans.emplace_back(lastnode->val);
         }
         return ans;
     }
