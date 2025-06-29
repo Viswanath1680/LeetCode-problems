f3a09f0a0b1fbd48bf1d32c67ac359fd222eb624
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-06-30 00:21:10 +0530

    Time: 0 ms (100%), Space: 22.1 MB (9.25%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,29 +1,35 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void inOrderTraversal( TreeNode* root, vector<int>& ans ){
-        if( root == nullptr )   return;
-        inOrderTraversal(root->left, ans);
-        ans.emplace_back(root->val);
-        inOrderTraversal(root->right, ans);
+    vector<int> morrisInorder(TreeNode* root){
+        vector<int> ans;
+        auto curr = root;
+        while( curr ){
+            if( curr->left == nullptr ){
+                ans.emplace_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                auto predecessor = curr->left;
+                while( predecessor->right && predecessor->right != curr  )    predecessor = predecessor->right;
+                if( predecessor->right == curr ){
+                    predecessor->right = nullptr;
+                    ans.emplace_back(curr->val);
+                    curr = curr->right;
+                }
+                else{
+                    predecessor->right = curr;
+                    curr = curr->left;
+                }
+            }
+        }
+        return ans;
     }
 
     bool isValidBST(TreeNode* root) {
-        vector<int> inOrder;
-        inOrderTraversal(root, inOrder);
-        for(int i = 1; i < inOrder.size(); i++ ){
-            if( inOrder[i] <= inOrder[i-1] )    return false;
-        }
+        if( !root ) return true;
+        vector<int> inorder = morrisInorder(root);
+        int n = inorder.size();
+        for( int i = 0; i < n-1; i++ )  if( inorder[i] >= inorder[i+1] )    return false;
         return true;
     }
 };
