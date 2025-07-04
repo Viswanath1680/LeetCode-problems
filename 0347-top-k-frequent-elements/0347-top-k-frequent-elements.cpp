810c08e3b7cc8af9f144eb68eeb8d481be64959d
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-07-05 01:56:42 +0530

    Time: 0 ms (100%), Space: 18 MB (51.99%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,20 +1,20 @@
+using pi = pair<int, int>;
+
 class Solution {
 public:
-    static bool comparator( pair<int,int> &a, pair<int,int> &b ){
-        return a.second > b.second;
-    }
-
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int, int> mp;
-        for( auto it : nums )
-            mp[ it ]++;
+        unordered_map<int, int> ump;
+        for( auto it : nums )   ump[it]++;
+        priority_queue<pi, vector<pi>, greater<pi>> minHeap;
+        for( auto p : ump ){
+            minHeap.push({p.second, p.first});
+            if( minHeap.size() > k )    minHeap.pop();
+        }
         vector<int> ans;
-        // mp.clear();
-        vector<pair<int,int>> arr;
-        for( auto it : mp )
-            arr.emplace_back( it );
-        sort( arr.begin(), arr.end(), comparator );
-        for( int i = 0 ; i < k ; i++ )  ans.emplace_back( arr[i].first );
+        while( !minHeap.empty() ){
+            ans.emplace_back( minHeap.top().second );
+            minHeap.pop();
+        }
         return ans;
     }
 };
