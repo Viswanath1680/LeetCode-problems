f5bdf18180ab7e1e577c7bd18d99695d2b87e93e
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-07-09 02:27:23 +0530

    Time: 25 ms (73.17%), Space: 19.4 MB (41.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,27 +1,41 @@
+// BFS way
+using pi = pair<int, int>;
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int i, int j) {
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == '0') {
-            return;
-        }
-        grid[i][j] = '0';
-        dfs(grid, i - 1, j);
-        dfs(grid, i + 1, j);
-        dfs(grid, i, j - 1);
-        dfs(grid, i, j + 1);
-    }
-
     int numIslands(vector<vector<char>>& grid) {
-        if ( grid.empty() ) return 0;
-        int ans = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == '1') {
-                    ans++;
-                    dfs(grid, i, j);
+        int m = grid.size(), n = grid[0].size();
+        auto copy = grid;
+        queue<pi> q;
+        auto bfs = [&](int i, int j) -> void{
+            copy[i][j] = '0';
+            q.push( {i, j} );
+            vector<pi> directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+            while( q.size() ){
+                int size = q.size();
+                while( size-- ){
+                    auto [r, c] = q.front(); q.pop();
+                    for( auto [dr, dc] : directions ){
+                        int new_r = r + dr, new_c = c + dc;
+                        if( new_r < 0 || new_r >= m || new_c < 0 || new_c >= n )    continue;
+                        if( copy[new_r][new_c] == '1' ){
+                            q.push( {new_r, new_c} );
+                            copy[new_r][new_c] = '0';
+                        }
+                    }
+                }
+            }
+        };
+
+
+        int islandCount = 0;
+        for( int i = 0; i < m; i++ ){
+            for( int j = 0; j < n; j++ ){
+                if( copy[i][j] == '1' ){
+                    islandCount++;
+                    bfs(i, j);
                 }
             }
         }
-        return ans;
+        return islandCount;
     }
 };
