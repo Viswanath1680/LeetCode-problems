7a68def235c45198afbc9af550e97d9c689f54fa
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-07-09 14:25:05 +0530

    Time: 0 ms (100%), Space: 15.1 MB (30.5%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,46 +1,46 @@
-// start from edges. If you find any 'O', apply bfs and store all the 'O's found from this edge 'O'. They should be remained as 'O'.
-// Make remaning 'O's as 'X
+// Start from edges. If you find any 'O', apply dfs and store all the 'O's found from this edge 'O'. They should be remained as 'O'.
+// Make remaning 'O's as 'X'
 using pi = pair<int, int>;
-
-struct hash_pair {
-    size_t operator()(const pi& p) const {
-        return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
-    }
-};
 
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
         int m = board.size(), n = board[0].size();
-        unordered_map<pi, bool, hash_pair> isConnectedToEdge;
+        vector<vector<bool>> isConnectedToEdge(m, vector<bool>(n, false));
 
-        auto dfs = [&](auto& self, int i, int j){
+        vector<pi> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        auto bfs = [&](int i, int j) -> void{
             if( i < 0 || i >= m || j < 0 || j >= n )    return;
             if( board[i][j] == 'X' )    return;
-            if( isConnectedToEdge.find( {i, j} ) != isConnectedToEdge.end() )   return;
-            isConnectedToEdge[ {i, j} ] = true;
-            self(self, i+1, j);
-            self(self, i-1, j);
-            self(self, i, j+1);
-            self(self, i, j-1);
+            if( isConnectedToEdge[i][j] )   return;
+            queue<pi> q;
+            q.push( {i, j} );
+            isConnectedToEdge[i][j] = true;
+            while( !q.empty() ){
+                auto [r, c] = q.front(); q.pop();
+                for( auto [dr, dc] : directions ){
+                    int new_r = r + dr, new_c = c + dc;
+                    if( new_r < 0 || new_r >= m || new_c < 0 || new_c >= n ) continue;
+                    if( board[new_r][new_c] == 'X' )    continue;
+                    if( isConnectedToEdge[new_r][new_c] )   continue;
+                    isConnectedToEdge[new_r][new_c] = true;
+                    q.push( {new_r, new_c} );
+                }
+            }
         };
 
         for( int i = 0; i < m; i++ )    {
-            if( board[i][0] == 'O' )    dfs(dfs, i, 0);
-            if( board[i][n-1] == 'O' )  dfs(dfs, i, n-1);
+            if( board[i][0] == 'O' )    bfs(i, 0);
+            if( board[i][n-1] == 'O' )  bfs(i, n-1);
         }
-        // for( int i = 0; i < m; i++ )    if( board[i][n-1] == 'O' )  dfs(dfs, i, n-1);
         for( int j = 0; j < n; j++ )    {
-            if( board[0][j] == 'O' )    dfs(dfs, 0, j);
-            if( board[m-1][j] == 'O' )  dfs(dfs, m-1, j);
+            if( board[0][j] == 'O' )    bfs(0, j);
+            if( board[m-1][j] == 'O' )  bfs(m-1, j);
         }
-        // for( int j = 0; j < n; j++ )    if( board[m-1][j] == 'O' )  dfs(dfs, m-1, j);
-
         for( int i = 0; i < m; i++ ){
             for( int j = 0; j < n; j++ ){
-                if( board[i][j] == 'O' ){
-                    if( isConnectedToEdge.find( {i, j} ) == isConnectedToEdge.end() )   board[i][j] = 'X';
-                }
+                if( board[i][j] == 'X' )    continue;
+                if( !isConnectedToEdge[i][j] )   board[i][j] = 'X';
             }
         }
     }
