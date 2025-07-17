9aabf4cc7b34f577b84a36dec1103db98c83788e
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-07-18 00:44:12 +0530

    Time: 56 ms (25.49%), Space: 66.3 MB (37.52%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,38 +1,29 @@
+// To use the topological sort algorithm, we will reverse every edge of the graph. Now, the nodes with indegree 0 become the terminal nodes. After this step, we will just follow the topological sort algorithm as it is.
 class Solution {
 public:
-    int n;
-    vector<int> degreein;
-    vector< vector<int> > rGraph;
-    vector<int> ans;
-    
-    void init(vector<vector<int>>& G){
-        n = G.size();
-        degreein = vector<int>(n,0);
-        rGraph.resize(n, vector<int>(0) );
-        
-        for (int u=0; u<n; u++){
-            for (int v:G[u]){
-                rGraph[v].push_back(u);
-                degreein[u]++;
+    vector<int> eventualSafeNodes(vector<vector<int>>& adjacencyList) {
+        int n = adjacencyList.size();
+        vector<vector<int>> reverseAdjList(n);
+        vector<int> indegree(n);
+        for( int node = 0; node < n; node++ ){
+            for( auto neighbor : adjacencyList[node]  ){
+                reverseAdjList[ neighbor ].emplace_back( node );
+                indegree[ node ]++;
             }
         }
-    }
-    
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        init(graph);
-        queue<int> que;
-        for (int i=0; i<n; i++){
-            if (degreein[i]==0) que.push(i);
-        }
-        while (que.size()){
-            int u = que.front(); que.pop();
-            ans.push_back(u);
-            for (int v:rGraph[u]){
-                degreein[v]--;
-                if (degreein[v]==0) que.push(v);
+
+        vector<int> topo;
+        queue<int> q;
+        for( int i = 0; i < n; i++ )    if( indegree[i] == 0 )  q.push( i );
+        while( !q.empty() ){
+            auto node = q.front(); q.pop();
+            topo.push_back( node );
+            for( auto neighbor : reverseAdjList[node] ){
+                indegree[neighbor]--;
+                if( indegree[neighbor] == 0 )   q.push( neighbor );
             }
         }
-        sort(ans.begin(),ans.end());
-        return ans;
+        sort(topo.begin(),topo.end());
+        return topo;
     }
 };
