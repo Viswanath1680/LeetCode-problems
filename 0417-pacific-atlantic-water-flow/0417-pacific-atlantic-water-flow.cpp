eb566818e97c6401e6557b212350e555e2602f95
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-07-25 10:59:00 +0530

    Time: 0 ms (100%), Space: 22.4 MB (78.2%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,34 +1,25 @@
 // Quoting Jacobi(Neetcode yt short https://youtube.com/shorts/4JAUsDqiV70?si=JLz2DcQ0QaAC6-I7), "Invert, always invert"
 using pi = pair<int, int>;
+using vvb = vector<vector<bool>>;
 class Solution {
 public:
-    vector<vector<int>> getIntersection(const set<pi>& pacific, const set<pi>& atlantic) {
-        vector<vector<int>> result;
-        for (const auto& point : pacific) {
-            if (atlantic.count(point)) {
-                result.push_back({point.first, point.second});
-            }
-        }
-        return result;
-    }
-
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int m = heights.size(), n = heights[0].size();
-
         vector<pi> directions = { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
         
-        auto dfs = [&](auto& self, set<pi>& s, int r, int c) -> void{
-            if( s.find( {r, c} ) != s.end() )   return;
-            s.insert( {r, c} );
+        auto dfs = [&](auto& self, vvb& visited, int r, int c) -> void{
+            if (visited[r][c]) return;
+            visited[r][c] = true;
             for( auto [dr, dc] : directions ){
                 auto nr = r + dr, nc = c + dc;
                 if (nr < 0 || nr >= m || nc < 0 || nc >= n) continue;
                 if (heights[nr][nc] < heights[r][c]) continue;
-                self(self, s, nr, nc);
+                self(self, visited, nr, nc);
             }
         };
 
-        set<pi> pacific, atlantic;
+        vvb pacific(m, vector<bool>(n, false));
+        vvb atlantic(m, vector<bool>(n, false));
         // cover pacific -> left, top
         for( int i = 0; i < n; i++ )    dfs(dfs, pacific, 0, i); // top
         for( int i = 0; i < m; i++ )    dfs(dfs, pacific, i, 0); // left
@@ -37,6 +28,13 @@ public:
         for( int i = 0; i < n; i++ )    dfs(dfs, atlantic, m-1, i);
         for( int i = 0; i < m; i++ )    dfs(dfs, atlantic, i, n-1);
 
-        return getIntersection(pacific, atlantic);
+        vector<vector<int>> result;
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (pacific[r][c] && atlantic[r][c])
+                    result.push_back({r, c});
+            }
+        }
+        return result;
     }
 };
