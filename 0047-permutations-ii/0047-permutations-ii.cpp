886a1a591bb8d411afcf3723013c5f9e052e0b11
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-07-27 01:44:36 +0530

    Time: 2 ms (73.11%), Space: 11.4 MB (90.95%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,24 +1,30 @@
+// Neetcode way
 class Solution {
-private:
-    void backtrack(vector<int>& arr, set<vector<int>>&s , int ind, int n){
-        if( ind == n )    {
-            s.insert(arr);
-            return;
-        }
-        for( int i = ind; i < n; i++ ){
-            swap(arr[i], arr[ind]);
-            backtrack(arr, s, ind+1, n);
-            swap(arr[i], arr[ind]);
-        }
-    }
-
 public:
-    vector<vector<int>> permuteUnique(vector<int>& arr) {
-        set<vector<int>> s;
-        int n = arr.size();
-        vector<vector<int>>ans;
-        backtrack(arr, s, 0, n);
-        for( auto it : s)   ans.emplace_back(it);
-        return ans;
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> answer;
+
+        auto dfs = [&](auto& self, vector<int>& arr, unordered_map<int, int>& ump) -> void{
+            if( arr.size() == n ){
+                answer.push_back(arr);
+                return;
+            }
+            for( auto [num, count] : ump ){
+                if( count == 0 )    continue;
+                ump[num]--;
+                // if( ump[num] == 0 ) ump.erase(num);  -> Causes heap-use-after-free bug
+                arr.emplace_back(num);
+                self(self, arr, ump);
+                arr.pop_back();
+                ump[num]++; // backtrack
+            }
+        };
+        
+        vector<int> arr;
+        unordered_map<int, int> ump;
+        for( auto it : nums )    ump[it]++;
+        dfs(dfs, arr, ump);
+        return answer;
     }
 };
