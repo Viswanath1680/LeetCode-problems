5e51809d6e277f68ea2635d0341b04ec0bb3e535
Viswanath.V <127508141+Viswanath1680@users.noreply.github.com>
2025-10-16 04:40:28 +0530

    Time: 15 ms (16.59%), Space: 11.3 MB (5.97%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,30 +1,26 @@
-/*
-Window can have atmost k chars which are not equal to most frequent character, for the answer to be maximum. Our goal is to maximise the len of the window.
-*/
+// Window can have atmost k chars which are not equal to most frequent character, for the answer to be maximum. Our goal is to maximise the len of the window.
 
 class Solution {
 public:
     int characterReplacement(string s, int k) {
         int l = 0, r = 0, len = 0, maxi = 0, ans = 0;
         // len is length of the window, which is (r-l+1)
-        map<char, int> mp;
+        unordered_map<char, int> mp;
         while( r < s.length() ){
             mp[ s[r] ]++;
             len++;
             maxi = max(maxi, mp[ s[r] ]);   // most frequent char in the window
-            if( len - maxi <= k )    ans = max(ans, len );
-            else if( len - maxi > k ){
-                while( len - maxi > k ){
-                    mp[ s[l] ]--;
-                    l++;
-                    len--;
-                    int temp_max = INT_MIN;
-                    for( auto it : mp ){
-                        temp_max = max( temp_max, it.second );
-                    }
-                    maxi = temp_max;
-                }
+            while( len - maxi > k ){
+                mp[ s[l] ]--;
+                if( mp[s[l]] == 0 ) mp.erase( s[l] );
+                l++;
+                len--;
+                int temp_max = INT_MIN;
+                for( auto it : mp )
+                    temp_max = max( temp_max, it.second );
+                maxi = temp_max;
             }
+            ans = max( ans, len );
             r++;
         }   
         return ans;
